a19d2e5efba01e6e06915b8ea08971646038ee35
zhou3388232 <2482891006@qq.com>
2015-12-28 14:49:51 +0800

    ajax cart

---
+++ Aaoo/Ajaxcart/Block/Cart/Index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 YourCompanyName . All rights reserved.
+ */
+namespace Aaoo\Ajaxcart\Block\Cart;
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+	
+}
